a8d9f04d8acdf43ab277c8abde9b95e51c0330b6
kpakert <kpakert@gmail.com>
2023-10-15 13:54:29 -0500

    got to opening the socket for listening

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -35,11 +35,16 @@ int main(int argc, char **argv)
     pid_t pid;
 
     serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
     memset(&server_sockaddr, 0, sizeof(server_sockaddr));
     server_sockaddr.sin_family = AF_INET;
     server_sockaddr.sin_port = htons(PORT);
     server_sockaddr.sin_addr.s_addr = INADDR_ANY;
-
+    
     // if d is specified launch as daemon
     if(argc==2 && argv[1][1] == 'd') 
     {
@@ -48,7 +53,30 @@ int main(int argc, char **argv)
         exit(-1);
       if (pid > 0)
         exit(0);
+    
     }
+    int opt = 1;
+	if (setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)) < 0) {
+        perror("setsockopt(SO_REUSEADDR) failed");
+	}
+
+    if (bind(serv_sock, (struct sockaddr *)&server_sockaddr, sizeof(server_sockaddr)) == -1) {
+        perror("bind");
+        close(serv_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGINT, sigint_handler);
+    signal(SIGTERM, sigint_handler);
+
+    // Listen for incoming connections
+    if (listen(serv_sock, 5) == -1) {
+        perror("listen");
+        close(serv_sock);
+        exit(EXIT_FAILURE);
+    }
+
+   
 
 
     return 0;
