f237ae3b00ac594d079bb985f44e68972a586af1
kpakert <kpakert@gmail.com>
2023-12-10 17:41:10 -0600

    missing terminating character...

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -21,7 +21,7 @@
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("kpakert); 
+MODULE_AUTHOR("kpakert"); 
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
